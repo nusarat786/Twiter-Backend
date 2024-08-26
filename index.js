8eee871797104f4b4dd35c9477a96634daff2f3f
HAVELIWALA NUSARATMAHENDI <95936097+nusarat786@users.noreply.github.com>
2024-08-26 15:06:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // cors 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000/' }));
 
 
 // using tweet routes
